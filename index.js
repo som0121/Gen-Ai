143fe7fee431d7191863e2ba8add7ebf64785de6
Som Sundar Parashar <somparashar007@gmail.com>
2026-09-12 15:52:01 +0530

    chat create history

---
+++ index.js
@@ -0,0 +1,33 @@
+import "dotenv/config";
+import { GoogleGenAI } from "@google/genai";
+import readlineSync from 'readline-sync';
+
+
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY
+});
+
+const chat = ai.chats.create({
+
+    model: "gemini-3.6-flash",
+    history:[],
+}
+
+)
+
+async function main(){
+
+  while (true) {
+
+  const userProblem = readlineSync.question("Ask me anything-->");
+  const response = await chat.sendMessage({
+
+    message: userProblem,
+  });
+
+  console.log(response.text)
+ 
+  }
+  }
+
+ main();
